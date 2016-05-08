a487bbc29f4e7fb7aa3806164a051da1aa260faa
Asefaw <asefaw.mekuria@gmail.com>
2016-05-07 20:11:28 -0400

    Updated Authentication

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -9,8 +9,12 @@ class CartController < ApplicationController
   	end
   end
 
+# the addItem method will add a product to the cart session using product id
+# if cart there is a cart with products use that cart 
+# otherwise the cart is empty
+
   def addItem
-  	item_id = params[:id]
+  	product_id = params[:id]
 	if session[:cart] then
 		cart = session[:cart]
 	else
@@ -18,21 +22,29 @@ class CartController < ApplicationController
 		cart = session[:cart]
 	end
 
-	#if item was already in the cart raise the qunatity'
-	if cart[item_id] then
-		cart[item_id] += 1
+	# check if item was already in the cart 
+  # if it is incerement the qunatity'
+  # otherwise quntitity is 1
+	if cart[product_id] then
+		cart[product_id] += 1
 	else
-		cart[item_id] = 1
+		cart[product_id] = 1
 	end
 	redirect_to :action => :index
   end
 
+ # empty the current cart with products in it
+ # and refresh the page by redirecting
   def clearCart
   	session[:cart] = nil
   	redirect_to :action => :index
   end
 
   #placing an order
+  # i'm tryng to palce the order by calling the placeOrder method in the Order Model
+  # but i'm problem getting the the values from the cart view 
+  order = Order.new
+  #order.palceOrder(cust_id, product_id, order_date, total)
   
   
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,15 @@ class Order < ActiveRecord::Base
   belongs_to :customer
   has_many :product_order
   has_many :products, through: :product_order
+
+
+
+#method below palces order by getting values from shopping cart through cart controller
+  def placeOrder(cust_id, pr_id, o_date, total)
+  	self.customer_id = cust_id
+  	self.product_id = pr_id
+  	self.orderDate = o_date
+  	self.totalCharge = total
+  	self.save
+  end
 end
